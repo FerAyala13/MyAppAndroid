b708d707de1aaa0cc75d0d84eae151180ca77e61
FerAyala13 <ca3542012019@unab.edu.sv>
2022-09-19 21:44:06 -0600

    commit

---
+++ app/src/main/java/com/example/myappandroid/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.myappandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        VariableyConstantes()
+        ejercicioVarVal()
+        tiposDeDatos()
+        tiposDeDatosDeducidosExplicitos()
+    }
+
+    private fun VariableyConstantes() {
+        var miPrimeraVariable ="Hola estudiantes de Programacion"
+        println(miPrimeraVariable)
+
+        miPrimeraVariable = "Aqui cambiamos"
+
+        //constante
+        val myPrimeraConstante = "Esto es una constante"
+        println(myPrimeraConstante)
+
+        val miSegundaConstante: String = miPrimeraVariable
+        println(miSegundaConstante)
+
+        var miNumero = 1
+        var miDouble = 2.2
+
+        }
+
+    }
+
+    fun ejercicioVarVal() {
+        println("Hola Alumnos")
+
+        val nombre: String = "Pedro"
+
+        var apellido: String = "Lopez"
+
+        var edad: Int = 21
+        edad = 22
+
+        var salario = 1200
+        salario = 1220
+
+        var nombreCompleto = nombre + " "+apellido
+        println(nombreCompleto)
+
+        val añoNacimiento = 2000
+        var añoActual = Calendar.getInstance().get(Calendar.YEAR)
+        var Edad = añoActual - añoNacimiento
+        println("Tu edad es: "+ Edad)
+
+
+    }
+
+    private fun tiposDeDatos(){
+        val myInt = 1
+        val myInt2 = 3
+        val myInt3: Int = myInt + myInt2
+        println(myInt3)
+
+        //DECIMALES
+
+        val myFloat = 1.7f
+        val myFloat2:Float = 1.7f
+        val myDouble = 1.3
+        val myDouble2:Double = 1.4
+        val myDouble3:Double = 5.0
+        val mySumaDouble = myDouble + myDouble2 + myDouble3
+        println(mySumaDouble)
+
+        //BOOLEAN
+
+        val myBoolean:Boolean = true
+        val myBoolean2 = false
+        val myBoolean3:Boolean = true
+        println(myBoolean==myBoolean2)
+        println(myBoolean&&myBoolean3)
+
+    }
+
+    private fun tiposDeDatosDeducidosExplicitos() {
+        var enteroExplicito: Int = 45
+        println(enteroExplicito.javaClass)
+        var enteroDeducido = 35
+        println(enteroDeducido.javaClass)
+
+        var DobleExplicito:Double = 45.45
+        println(DobleExplicito.javaClass)
+        var DobleDeducido = 35
+        println(DobleDeducido.javaClass)
+    }
